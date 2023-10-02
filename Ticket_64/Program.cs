a9f1ed34dfcd64fd2e1fa04dd70f2a277c20de82
владимир назаров <volottnazarov@mail.ru>
2023-10-03 00:36:20 +0300

    Все натурал. числа в промежутке от N до 1. Рекурсия

---
+++ Ticket_64/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+
+
+
+int NaturalNumbers(int N)
+{
+if (N == 0) return 1 ; 
+Console.Write(N+",");
+return NaturalNumbers(N - 1);
+}
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+if(n == 0) Console.WriteLine("Ошибка! Введите число больше 0 ");
+Console.Write($"N = {n} -> " );
+NaturalNumbers(n);   
